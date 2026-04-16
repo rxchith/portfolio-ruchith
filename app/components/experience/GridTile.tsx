28a11388cc5d83483c55b76f3a29db79579f3c2b
RuRam <apple@MacBook-Pro.local>
2026-04-16 11:20:14 +0530

    fix: use ts-expect-error instead of ts-ignore

--- app/components/experience/GridTile.tsx
+++ app/components/experience/GridTile.tsx
@@ -203,7 +203,7 @@ const GridTile = (props: GridTileProps) => {
       </mesh>
 
       {/* 2. Overlays (Thumbnails, Titles, Hover Decorations) */}
-      {/* @ts-ignore - disable raycasting on overlay group */}
+      {/* @ts-expect-error - disable raycasting on overlay group */}
       <group raycast={() => null}>
         {/* Thumbnail: slightly in front of portal to prevent Z-fighting */}
         <mesh ref={thumbnailRef} position={[0, 0, 0.05]}>
